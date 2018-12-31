a4d6fd430b634af506be743088f91d80f2d8bda5
tman5000 <39314370+tman5000@users.noreply.github.com>
2018-12-31 13:18:48 +0000

    added get album method

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -8,4 +9,7 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map((response) => response.json());
+  }
 }
