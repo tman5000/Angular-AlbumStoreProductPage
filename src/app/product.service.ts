020777168100d401c2b9007d26ccad59dbfa3b19
tman5000 <39314370+tman5000@users.noreply.github.com>
2018-12-31 13:12:49 +0000

    added field

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,6 +4,8 @@ import { Http, Response} from '@angular/http';
 @Injectable()
 export class ProductService {
 
+  private _albumUrl = '../assets/album.json';
+
   constructor(private _http: Http) { }
 
 }
