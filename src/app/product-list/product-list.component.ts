c152af67df20faaa2937771d9c129ea93c4d73ea
tman5000 <39314370+tman5000@users.noreply.github.com>
2018-12-31 19:24:02 +0000

    added service for product

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from '../product';
+import {ProductService} from '../product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products: Product[];
+
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
   }
